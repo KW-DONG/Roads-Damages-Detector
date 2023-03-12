634f2933387eb420d085ff1bd48cc3b7aca97127
Kaiwen Dong <42641126+KW-DONG@users.noreply.github.com>
2023-03-12 20:24:30 +0000

    Update monitor.h

--- src/monitor.h
+++ src/monitor.h
@@ -1,11 +1,79 @@
 #ifndef MONITOR_H
 #define MONITOR_H
 
-#include <qnamespace.h>
+#include <QObject>
+#include <QStringList>
+#include <QImage>
+#include <QMutex>
+#include "resultlistdata.h"
+#include "tasklistdata.h"
+#include "camera.h"
 
-enum TaskRole {
-    TitleRole = Qt::UserRole,
-    DescriptionRole
+class Monitor : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(int currentTask READ currentTask WRITE setCurrentTask NOTIFY currentTaskChanged)
+    Q_PROPERTY(QStringList taskList READ taskList NOTIFY taskListChanged)
+    Q_PROPERTY(QImage img READ img NOTIFY imgChanged)
+    Q_PROPERTY(bool run READ run NOTIFY runChanged)
+
+public:
+    Monitor();
+
+    void runDetection(const cv::Mat& mat);
+
+    struct MyCallback : Camera::SceneCallback {
+        Monitor* monitor = nullptr;
+        virtual void nextScene(const cv::Mat &mat) {
+                    if (nullptr != monitor) {
+                        monitor->runDetection(mat);
+                    }
+                }
+    };
+
+signals:
+    void currentTaskChanged();
+    void localImgPathChanged();
+    void logChanged();
+    void taskListChanged();
+    void runChanged();
+
+    void imgChanged();
+
+public slots:
+    void setCurrentTask(int i);
+    int currentTask();
+
+    void setLocalImgPath(QString path);
+    QString localImgPath();
+
+    QStringList log();
+    QStringList taskList();
+
+    bool run();
+    void runButton();
+
+    void setTaskListData(TaskListData* ptr);
+    void setResultListData(ResultListData* ptr);
+
+    QImage img();
+
+private:
+    Camera* camera;
+    TaskListData* pTaskListData;
+    ResultListData* pResultListData;
+
+    //properties
+    int _currentTask;
+    bool _run;
+    QString _localImgPath;
+    QStringList _log;
+    QMutex mutex;
+    QImage _img;
+
+    MyCallback myCallback;
+
 };
 
 #endif
