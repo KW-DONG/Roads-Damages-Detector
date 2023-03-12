0b710637d31a175cab1d3f669abdf4669bbfa875
Kaiwen Dong <42641126+KW-DONG@users.noreply.github.com>
2023-03-12 20:24:26 +0000

    Update qpaintitem.cpp

--- src/qpaintitem.cpp
+++ src/qpaintitem.cpp
@@ -1,25 +1,34 @@
 #include "qpaintitem.h"
 #include <iostream>
 
-PaintItem::PaintItem(QQuickItem *parent) : QQuickItem(parent)
+PaintItem::PaintItem(QQuickPaintedItem *parent) : QQuickPaintedItem(parent)
 {
     //这句不加会报错
     setFlag(ItemHasContents, true);
     //默认图片
     //m_imageThumb = QImage(":/Image/background.png");
+
+    m_timer = new QTimer(this);
+
+    connect(m_timer, &QTimer::timeout, this, [this](){
+        update();
+    });
+    m_timer->start(100);
 }
 
 PaintItem::~PaintItem()
 {
+    m_timer->stop();
     std::cout << "paint item destroyed" << std::endl;
 }
 
 void PaintItem::updateImage(const QImage &image)
 {
     m_imageThumb = image;
-    emit widthChanged();
-    emit heightChanged();
-    update();
+    //emit widthChanged();
+    //emit heightChanged();
+    //update();
+    //std::cout << "paint item update" << std::endl;
 }
 
 
@@ -36,17 +45,14 @@ QSGNode * PaintItem::updatePaintNode(QSGNode *oldNode, QQuickItem::UpdatePaintNo
     node->setRect(boundingRect());
     node->markDirty(QSGNode::DirtyForceUpdate);
     node->setTexture(m_texture);
-
     return node;
 }
 
-/*
 void PaintItem::paint(QPainter *painter)
 {
     painter->drawImage(this->boundingRect(), m_imageThumb);
+    static int i = 0;
 }
-*/
-
 
 
 int PaintItem::getHeight()
