35a489def3527235533cf9244fb526bcf09e7926
Kaiwen Dong <42641126+KW-DONG@users.noreply.github.com>
2023-03-12 20:24:56 +0000

    Create camera.h

---
+++ src/driver/camera.h
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <thread>
+
+class Camera {
+public:
+	struct SceneCallback {
+		virtual void nextScene(const cv::Mat &mat) = 0;
+	};
+
+	Camera() = default;
+
+	void start(int deviceID = 0, int apiID = 0);
+	void stop();
+	void registerSceneCallback(SceneCallback* sc) {
+		sceneCallback = sc;
+	}
+    
+private:
+    void getFrame();
+	void threadLoop();
+    cv::VideoCapture cap;
+	std::thread cameraThread;
+    bool run = false;
+	SceneCallback* sceneCallback = nullptr;
+};
