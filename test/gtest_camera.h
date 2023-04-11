e108cc5ac909cd3f5511929569a8ce4d4cc20529
Kaiwen Dong <42641126+KW-DONG@users.noreply.github.com>
2023-04-11 16:45:47 +0100

    Create gtest_camera.h

---
+++ test/gtest_camera.h
@@ -0,0 +1,37 @@
+#ifndef GTEST_CAMERA_H
+#define GTEST_CAMERA_H
+#include <gtest/gtest.h>
+#include "camera.h"
+
+class Test_Camera
+{
+public:
+    bool checkIsStart(int id)
+    {
+        mCam.start(id);
+        bool result = mCam.run == true;
+        mCam.stop();
+        return result;
+    }
+    bool checkIsStop(int id)
+    {
+        mCam.start(id);
+        mCam.stop();
+        return mCam.run == false;
+    }
+    Camera mCam;
+};
+
+TEST(camera_test, checkIsStart)
+{
+    Test_Camera test;
+    EXPECT_EQ(test.checkIsStart(0), true);
+}
+
+TEST(camera_test, checkIsStop)
+{
+    Test_Camera test;
+    EXPECT_EQ(test.checkIsStop(0), true);
+}
+
+#endif
